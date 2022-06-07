1d40e606055761a1cb16f353dd1ad9a68c11d213
Laukó Tamás <laukotomi@gmail.com>
2022-06-07 12:18:56 +0200

    Removed EFCore to resolve git directory naming issue part2

---
+++ src/LTest.EfCore/Hooks/CleanDatabaseHook.cs
@@ -0,0 +1,46 @@
+﻿using LTest.EFCore.Services;
+using LTest.Helpers;
+using LTest.Hooks;
+using LTest.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace LTest.EFCore.Hooks
+{
+    /// <summary>
+    /// Cleans database using EF features.
+    /// </summary>
+    public class CleanDatabaseHook<TDbContext> : IBeforeTestHook
+        where TDbContext : DbContext
+    {
+        private readonly TDbContext _dbContext;
+        private readonly DatabaseCleanupService _databaseCleanupService;
+        private readonly ITestLogger _testLogger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CleanDatabaseHook{TDbContext}"/> class.
+        /// </summary>
+        /// <param name="dbContext">The db context.</param>
+        /// <param name="databaseCleanupService">Database cleanup service.</param>
+        /// <param name="testLogger">Logger.</param>
+        public CleanDatabaseHook(
+            TDbContext dbContext,
+            DatabaseCleanupService databaseCleanupService,
+            ITestLogger testLogger)
+        {
+            _dbContext = dbContext;
+            _databaseCleanupService = databaseCleanupService;
+            _testLogger = testLogger;
+        }
+
+        /// <inheritdoc/>
+        public async Task BeforeTestAsync()
+        {
+            var elapsedMs = await StopwatchHelper.MeasureAsync(async () =>
+            {
+                await _databaseCleanupService.CleanupAsync(_dbContext);
+            });
+
+            _testLogger.LogInformation($"DB cleaned ({elapsedMs} ms)");
+        }
+    }
+}
